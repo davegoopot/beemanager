33c9068ec59937c4bd10d51b3097cf7c3dbee63a
Dave Potts <dave@davepotts.software>
2025-01-18 11:21:01 +0000

    Saving picture to subfolder.

--- takepic.py
+++ takepic.py
@@ -3,15 +3,20 @@
     `uv run takepic.py` from the command line.
 """
 
-try:
-    import picamera2
-except Exception as ex:
-    print(ex)
+import os
+import picamera2
 
-camera = picamera2.Picamera2()
-still_config = camera.create_still_configuration(main={"size": (640, 480)})
-camera.configure(still_config)
-camera.start()
-filename = 'testimage.jpg'
-camera.capture_file(filename)
-print(f'Image saved as {filename}')
+def take_picture(filname: str):
+    camera = picamera2.Picamera2()
+    still_config = camera.create_still_configuration(main={"size": (640, 480)})
+    camera.configure(still_config)
+    camera.start()
+    camera.capture_file(filename)
+    print(f'Image saved as {filename}')
+    camera.close()
+
+if __name__ == "__main__":
+    picture_folder = "pics"
+    os.makedirs(picture_folder, exist_ok=True)
+    filename = os.path.join(picture_folder, "testimage.jpg")
+    take_picture(filename)
