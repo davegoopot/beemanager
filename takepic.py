fcd46f091844c7fe04619e49afc9031af5191866
Dave Potts <dave@davepotts.software>
2025-01-18 11:11:11 +0000

    Simple picture taking

--- takepic.py
+++ takepic.py
@@ -1,20 +1,17 @@
 """
     Simple script to take a picture with the Raspberry Pi camera. Intended to be run using 
     `uv run takepic.py` from the command line.
-
-    To make the uv dependencies work, we add inline script metadata.
 """
 
-# /// script
-# dependencies = [
-#   "picamera"
-# ]
-# ///
+try:
+    import picamera2
+except Exception as ex:
+    print(ex)
 
-import picamera
-
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
+camera = picamera2.Picamera2()
+still_config = camera.create_still_configuration(main={"size": (640, 480)})
+camera.configure(still_config)
+camera.start()
 filename = 'testimage.jpg'
-camera.capture(filename)
+camera.capture_file(filename)
 print(f'Image saved as {filename}')
